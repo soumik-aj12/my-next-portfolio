411a0ef544e4ab058f5ebfc4526bca7e0d5e4a6e
Soumik Sil <soumiksilco@gmail.com>
2024-07-29 21:47:39 +0530

    skill issue updated

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -8,7 +8,7 @@ export function ProjectCard({ project }: any) {
       <div className="flex flex-col items-center justify-between p-8">
         <div className="mb-4 text-sm text-center lg:text-2xl">{project.name}</div>
         <div className="text-justify text-sm">{project.description}</div>
-        <div className="flex flex-row">
+        <div className="grid grid-cols-2 md:flex flex-row">
           {project.technology.map((tech:String,index:any)=>{
             return (
               <div key={index} className="p-2 w-full">
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -3,7 +3,7 @@
 import { FaCss3, FaHtml5, FaNodeJs, FaPhp, FaReact } from "react-icons/fa";
 import { SiExpress, SiMongodb, SiMysql, SiNextdotjs, SiTailwindcss, SiTypescript } from "react-icons/si";
 
-const size = "h-10 w-10";
+const size = "h-10 w-10 p-1 rounded-lg hover:scale-110";
 
 
 export const ProjectsList= [
@@ -11,7 +11,7 @@ export const ProjectsList= [
     name: "daily expense tracker",
     description:
       "an intuitive application that helps you track your daily expenses effortlessly. Record your spending, categorize expenses, and gain insights into your financial habits to stay on top of your budget and savings goals.",
-    technology: [<FaHtml5 className={`${size} bg-orange-600 h-10 w-10`}/>, <FaCss3 className={`${size} bg-blue-400`}/>, <FaPhp className={`${size} bg-violet-400`}/>, <SiMysql className={`${size} text-orange-600`}/>],
+    technology: [<FaHtml5 className={`${size} bg-orange-600 h-10 w-10`}/>, <FaCss3 className={`${size} bg-blue-400`}/>, <FaPhp className={`${size} bg-violet-400`}/>, <SiMysql className={`${size} text-orange-600 bg-blue-900`}/>],
     github: "https://github.com/soumik-aj12/Daily-Expense-Tracker",
   },
   {
