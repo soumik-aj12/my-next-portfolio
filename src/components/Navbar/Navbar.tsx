51fdd47d7523c937dada1c5fca58b41cbd491c56
Soumik Sil <soumiksilco@gmail.com>
2024-08-30 17:05:34 +0530

    minor fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,9 +5,17 @@ import { Button } from "../ui/button";
 const Navbar = () => {
   return (
     <nav className="flex justify-around items-center pt-8">
-      <h2 className="text-4xl font-semibold"><span className="text-green-500">5</span>am.</h2>
-
-      <Button className="hover:bg-white/20 hover:text-white"><Link href="https://mega.nz/file/RY1XzZgZ#rROHoUcBhrIEqgLvHs9HJTwbxzxOAmOK4RGcaJinRu0" target="_blank">Download Resume</Link></Button>
+      <h2 className="text-4xl font-semibold">
+        <span className="text-green-500">5</span>am.
+      </h2>
+      <Link
+        href="https://mega.nz/file/RY1XzZgZ#rROHoUcBhrIEqgLvHs9HJTwbxzxOAmOK4RGcaJinRu0"
+        target="_blank"
+      >
+        <Button className="hover:bg-white/20 hover:text-white">
+          Download Resume
+        </Button>
+      </Link>
     </nav>
   );
 };
